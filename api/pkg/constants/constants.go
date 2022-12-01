0a1ceb0ea40f2d4864eabae0433d842f99027df0
Timofey Maltsev <99428739+SiropTime@users.noreply.github.com>
2022-12-02 01:45:51 +0300

    del

--- api/pkg/constants/constants.go
+++
@@ -1,7 +0,0 @@
-package constants
-
-const PostgresDriver = "postgres"
-const PostgresUrl = "host=localhost port=5433 user=timofey password=busa2012 dbname=portfolio_db sslmode=disable"
-
-const SwapAPIURL = "https://amaze.finance/api/swap/"
-const NativeAddress = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
