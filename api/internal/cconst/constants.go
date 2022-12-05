04a5c652503e8aeaa7a0d84f3240a9b67acef3aa
SiropTime <sirop4beats@gmail.com>
2022-12-05 13:52:49 +0300

    [+] Some environment refactoring with ports and created migrations (not sure about it)

--- api/internal/cconst/constants.go
+++ api/internal/cconst/constants.go
@@ -7,8 +7,8 @@ import (
 
 const PostgresDriver = "postgres"
 
-var PostgresUrl = fmt.Sprintf("host=postgresql port=5432 user=%s password=%s dbname=%s sslmode=disable",
-	os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
+var PostgresUrl = fmt.Sprintf("host=postgresql port=%s user=%s password=%s dbname=%s sslmode=disable",
+	os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"))
 
 const SwapAPIURL = "https://amaze.finance/api/swap/"
 const NativeAddress = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
